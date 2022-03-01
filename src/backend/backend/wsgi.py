78a56eb2f5df71f8c27ec53512a72c94a1355590
HVKukkonen <software.kukkonen@gmail.com>
2022-03-01 19:30:50 +0200

    containerised production build

--- src/backend/backend/wsgi.py
+++ src/backend/backend/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+# own
+src_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+sys.path.insert(0, src_dir)
 
 application = get_wsgi_application()
